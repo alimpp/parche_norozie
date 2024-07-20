81cd78e4726c2f4db49c8ddd6cae478c43cdc3c0
alimpp <hmdmpp@gmail.com>
2024-07-20 05:22:43 -0700

    Add scss file in assets dir

--- app.vue
+++ app.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>پارچه نوروزی</div>
+  <div class="app-bg-danger app-w-100">
+    <span class="app-color-primary app-font-size-20 app-font-weight-900"
+      >Hello world</span
+    >
+    <UserIcon size="1" class="app-color-primary"></UserIcon>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+
   css: [
     "@/assets/scss/main.scss",
     // "vuetify/lib/styles/main.sass",
     // "@mdi/font/css/materialdesignicons.css",
   ],
+
+  modules: ["nuxt-feather-icons"],
 });
