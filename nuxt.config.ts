2366f5f951cb1f4629544fb66c08ad5212d3a464
alimpp <hmdmpp@gmail.com>
2024-08-20 12:53:51 -0700

    False ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
-  ssr: true,
+  ssr: false,
   runtimeConfig: {
     public: {
       baseUrl: process.env.BASE_URL,
