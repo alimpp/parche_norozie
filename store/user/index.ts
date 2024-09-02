c369f9bd0deacf2bd617fcb65ca5bca651136576
mahdishoaei <mahdishoaei98@gmail.com>
2024-09-02 21:49:12 +0330

    change request of api from nuxt server to liara

--- store/user/index.ts
+++ store/user/index.ts
@@ -14,29 +14,29 @@ export const useUserStore = defineStore("useUserStore", {
       const cookie = useCookie("token");
 
       if (cookie.value) {
-        await $fetch("/api/v1/profile")
-          .then((res: any) => {
-            authStore.isAuthenticated = true;
-            this.user = res.data;
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-
-        // await $fetch("https://parche-go.liara.run/api/v1/profile", {
-        //   headers: {
-        //     Authorization: `Bearer ${cookie.value}`,
-        //     "Content-Type": "application/json",
-        //   },
-        // })
+        // await $fetch("/api/v1/profile")
         //   .then((res: any) => {
-        //     console.log("res", res);
         //     authStore.isAuthenticated = true;
         //     this.user = res.data;
         //   })
         //   .catch((err) => {
-        //     authStore.isAuthenticated = false;
+        //     console.log(err);
         //   });
+
+        await $fetch("https://parche-go.liara.run/api/v1/profile", {
+          headers: {
+            Authorization: `Bearer ${cookie.value}`,
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res: any) => {
+            console.log("res", res);
+            authStore.isAuthenticated = true;
+            this.user = res.data;
+          })
+          .catch((err) => {
+            authStore.isAuthenticated = false;
+          });
       } else {
         authStore.isAuthenticated = false;
       }
@@ -45,31 +45,31 @@ export const useUserStore = defineStore("useUserStore", {
     async sendProfile(param: any) {
       const cookie = useCookie("token");
 
-      await useFetch("/api/v1/profile/update", {
-        method: "PUT",
-        body: param,
-      })
-        .then((res) => {
-          this.userProfile();
-        })
-        .catch((err) => {
-          err;
-        });
-
-      // await $fetch("https://parche-go.liara.run/api/v1/profile/update", {
+      // await useFetch("/api/v1/profile/update", {
       //   method: "PUT",
       //   body: param,
-      //   headers: {
-      //     Authorization: `Bearer ${cookie.value}`,
-      //     "Content-Type": "application/json",
-      //   },
       // })
-      //   .then((res: any) => {
+      //   .then((res) => {
       //     this.userProfile();
       //   })
       //   .catch((err) => {
-      //     console.log(err);
+      //     err;
       //   });
+
+      await $fetch("https://parche-go.liara.run/api/v1/profile/update", {
+        method: "PUT",
+        body: param,
+        headers: {
+          Authorization: `Bearer ${cookie.value}`,
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res: any) => {
+          this.userProfile();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 });
