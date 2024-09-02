e751b6ef6b320879b3875b3a92725d396dc3d788
alimpp <hmdmpp@gmail.com>
2024-09-02 14:46:02 -0700

    Revert changes

--- store/address/index.ts
+++ store/address/index.ts
@@ -64,5 +64,63 @@ export const useAddressStore = defineStore("useAddressStore", {
           console.log(err);
         });
     },
+
+    async update(param: any) {
+      const cookie = useCookie("token");
+
+      // await $fetch("/api/v1/address/update", {
+      //   method: "PUT",
+      //   body: param,
+      // })
+      //   .then((res) => {
+      //     this.getAddress();
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+
+      await $fetch("https://parche-go.liara.run/api/v1/address/update", {
+        method: "PUT",
+        body: param,
+        headers: {
+          Authorization: `Bearer ${cookie.value}`,
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          this.getAddress();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    async delete(param: any) {
+      const cookie = useCookie("token");
+
+      // await $fetch(`/api/v1/address/${param.uuid}`, {
+      //   method: "DELETE",
+      // })
+      //   .then((res) => {
+      //     this.getAddress();
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+
+      await $fetch(`https://parche-go.liara.run/api/v1/address/${param.uuid}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${cookie.value}`,
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          this.getAddress();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
 });
