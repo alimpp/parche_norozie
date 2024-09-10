4c8468d18dc43e6dd23c0f2b4fa3f153ad08b364
mahdishoaei <mahdishoaei98@gmail.com>
2024-09-10 23:32:20 +0330

    about-us page fill and finished

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="app-container">
-    <!-- <v-row>
-      <v-col md="7" sm="12" xs="4">
-        <div class="app-w-100 app-px-2 app-py-2">
-          <span
-            >فروشگاه نوروزی از سال۹۸ بصورت فروشگاه حظوری شروع به فعالیت
-            کرده،احتمالا منتظر باشین که بگم از یه فروشگاه کوچیک ولی اینطور نیست
-            بلکه از یه فروشگاه بزرگ ولی منطقه تقریبا ضعیف…
-          </span>
-        </div>
-      </v-col>
-      <v-col md="5" sm="12" xs="4">
-        <div class="app-w-100 app-px-2 app-py-2">
-          <AboutUsCarousel :dataSource="sectionOne" />
-        </div>
-      </v-col>
-    </v-row> -->
+  <div class="app-container app-flex">
+    <div class="app-w-100 app-px-2 app-py-2 text">
+      <span> {{ $t("about us page text 1") }} </span>
+      <br />
+      <br />
+      <span> {{ $t("about us page text 2") }} </span>
+      <br />
+      <br />
+      <span> {{ $t("about us page text 3") }}</span>
+    </div>
+    <div class="app-w-100 app-px-2 mt-6 app-py-2 carousel">
+      <AboutUsCarousel :dataSource="sectionOne" />
+    </div>
+  </div>
+  <div class="app-flex app-flex-column img">
+    <img src="/assets/images/fabric2.jpg" alt="fabric" class="app-w-100" />
   </div>
 </template>
 
 <script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
 const sectionOne = ref([
   {
     id: 1,
@@ -43,3 +45,21 @@ const sectionOne = ref([
   },
 ]);
 </script>
+
+<style scoped>
+span {
+  text-wrap: wrap;
+}
+@media (max-width: 800px) {
+  .app-container {
+    display: flex;
+    flex-direction: column;
+  }
+  .text {
+    width: 100%;
+  }
+  .carousel {
+    width: 100%;
+  }
+}
+</style>
