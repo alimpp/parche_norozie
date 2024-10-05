46e6db0e09d1776640ece9f43ea8dfbbfed6b264
mahdishoaei <mahdishoaei98@gmail.com>
2024-10-05 21:06:43 +0330

    create base structure of blog admin

---
+++ components/Modals/Blog.vue
@@ -0,0 +1,37 @@
+<template>
+  <AppModal
+    width="90%"
+    :state="state"
+    @close="emit('close')"
+    :title="$t('create new blog')"
+  >
+    <template #content>
+      <div class="app-flex app-flex-column app-px-2 app-py-2">
+        <div class="app-flex app-flex-column app-w-100">
+          <AppInput height="35px" :label="$t('title')" />
+          <AppInput height="35px" :label="$t('upload img')" class="" />
+          <AppInput height="35px" :label="$t('subject')" />
+          <AppTextarea :label="$t('text')" />
+          <AppButton
+            width="150px"
+            class="app-mx-2 app-mt-11"
+            :name="$t('create')"
+            background="app-bg-primary"
+            height="35px"
+          />
+        </div>
+      </div>
+    </template>
+  </AppModal>
+</template>
+
+<script setup>
+const emit = defineEmits(["close"]);
+
+const props = defineProps({
+  state: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
--- pages/admin/blogs.vue
+++ pages/admin/blogs.vue
@@ -1,7 +1,49 @@
-<template>blogs</template>
+<template>
+  <AppCard class="app-mt-3">
+    <template #content>
+      <div class="app-flex app-flex-column app-px-2 app-py-2">
+        <span class="app-font-size-16">{{ $t("blogs") }}</span>
+        <div class="app-flex app-w-100">
+          <AppInput height="35px" :label="$t('title')" />
+          <AppButton
+            class="app-mx-2 app-mt-11"
+            :name="$t('search')"
+            background="app-bg-primary"
+            icon="search"
+            height="35px"
+          />
+        </div>
+        <AppButton
+          class="app-mx-2 app-mt-11"
+          :name="$t('create new blog')"
+          background="app-bg-primary"
+          height="35px"
+          width="150px"
+          @click="openBlogModal"
+        />
+        <ModalsBlog
+          :state="blogModalState"
+          @close="blogModalState = false"
+        />
+      </div>
+    </template>
+  </AppCard>
+
+ 
+</template>
 <script setup>
+import Address from '~/components/Cards/Address.vue';
+
 definePageMeta({
   middleware: ["admin-auth"],
   layout: "admin",
 });
+
+const blogModalState = ref(false);
+const openBlogModal = ()=>{
+  blogModalState.value = true;
+}
+
 </script>
+
+<style scoped></style>
