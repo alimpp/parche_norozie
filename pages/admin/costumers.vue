f56d4a2c7fb9ee27f746bf2ec7d7dd12bdb3e61c
mahdishoaei <mahdishoaei98@gmail.com>
2024-10-27 16:27:53 +0330

    add modal to seach costumers form

--- pages/admin/costumers.vue
+++ pages/admin/costumers.vue
@@ -2,44 +2,67 @@
   <div class="app-w-100">
     <AppCard class="app-mt-3">
       <template #content>
-        <div class="app-flex app-flex-column app-px-2 app-py-2">
-          <AppBeardCrumb :route="$t('dashboard')" :child="$t('costumers')" />
-          <div class="app-flex app-w-100">
-            <AppInput
-              height="35px"
-              :label="$t('phoneNumber')"
-              v-model="filter.phone"
-            />
-            <AppInput
-              height="35px"
-              :label="$t('name')"
-              class="app-mx-2"
-              v-model="filter.name"
-            />
-            <AppInput
-              height="35px"
-              :label="$t('family name')"
-              v-model="filter.lastname"
-            />
-            <AppInput
-              height="35px"
-              :label="$t('email address')"
-              class="app-mx-2"
-              v-model="filter.email"
-            />
-            <AppInput
-              height="35px"
-              :label="$t('id card')"
-              v-model="filter.id_card_number"
-            />
+        <div class="app-flex app-align-center app-px-2 app-py-4">
+          <div class="app-w-50">
+            <AppBeardCrumb :route="$t('dashboard')" :child="$t('costumers')" />
+          </div>
+          <div class="app-w-50 app-flex app-justify-end">
             <AppButton
-              class="app-mx-2 app-mt-11"
-              :name="$t('search')"
+              :name="$t('search') + ' ' + $t('costumers')"
               background="app-bg-primary"
-              icon="search"
               height="35px"
-              @click="search"
+              @click="openModal"
             />
+          </div>
+          <div>
+            <AppModal
+              width="90%"
+              :state="ModalState"
+              @close="ModalState = false"
+              :title="$t('filter by')"
+            >
+              <template #content>
+                <div class="app-flex app-flex-column app-px-4 app-py-8">
+                  <div class="app-flex app-w-100">
+                    <AppInput
+                      height="35px"
+                      :label="$t('phoneNumber')"
+                      v-model="filter.phone"
+                    />
+                    <AppInput
+                      height="35px"
+                      :label="$t('name')"
+                      class="app-mx-2"
+                      v-model="filter.name"
+                    />
+                    <AppInput
+                      height="35px"
+                      :label="$t('family name')"
+                      v-model="filter.lastname"
+                    />
+                    <AppInput
+                      height="35px"
+                      :label="$t('email address')"
+                      class="app-mx-2"
+                      v-model="filter.email"
+                    />
+                    <AppInput
+                      height="35px"
+                      :label="$t('id card')"
+                      v-model="filter.id_card_number"
+                    />
+                    <AppButton
+                      class="app-mx-2 app-mt-11"
+                      :name="$t('search')"
+                      background="app-bg-primary"
+                      icon="search"
+                      height="35px"
+                      @click="search"
+                    />
+                  </div>
+                </div>
+              </template>
+            </AppModal>
           </div>
         </div>
       </template>
@@ -99,6 +122,11 @@ const filter = ref({
 
 const search = () => {
   customersStore.getAllCustomers(filter.value);
+};
+
+const ModalState = ref(false);
+const openModal = () => {
+  ModalState.value = true;
 };
 
 onMounted(async () => {
