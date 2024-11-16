47339197453e20901f253509e564ae89fb12bd60
alimpp <hmdmpp@gmail.com>
2024-11-16 02:30:53 -0800

    New changes in upload file

--- components/File/Image.vue
+++ components/File/Image.vue
@@ -11,8 +11,18 @@
       :class="{ 'app-mx-4': urlImage }"
       class="app-pointer app-border app-w-100 app-border-radius app-flex app-flex-column app-justify-center app-align-center app-h-300-px"
     >
-      <span class="f-s-16 f-w-600">{{ $t("upload image") }}</span>
-      <ImageIcon size="1.5x"></ImageIcon>
+      <v-progress-circular
+        indeterminate
+        color="#7272f5"
+        v-if="loading"
+      ></v-progress-circular>
+      <div
+        class="app-flex app-flex-column app-justify-center app-align-center"
+        v-else
+      >
+        <span class="f-s-16 f-w-600">{{ $t("upload image") }}</span>
+        <ImageIcon size="1.5x"></ImageIcon>
+      </div>
     </div>
   </div>
 </template>
@@ -28,16 +38,23 @@ const emit = defineEmits(["fileInformation"]);
 
 const file = ref(null);
 const fileElement = ref();
+const loading = ref(false);
 
 const openFile = () => {
   fileElement.value.click();
 };
 
 const upload = async (event) => {
+  loading.value = !loading.value;
   const fileName = generateUid(30);
   file.value = event.target.files[0];
   const urlImage = URL.createObjectURL(file.value);
-  await fileStore.upload(file.value, `${fileName}.png`);
-  emit("fileInformation", { fileName: `${fileName}.png`, urlImage });
+  const type = file.value.type.split("/")[1];
+  await fileStore.upload(file.value, `${fileName}.${type}`);
+  emit("fileInformation", {
+    fileName: `${fileName}.${type}`,
+    urlImage,
+  });
+  loading.value = !loading.value;
 };
 </script>
