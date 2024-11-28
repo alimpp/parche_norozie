f57bde4223e7fb44a2e4a5ac7a0a3d9d763957af
alimpp <hmdmpp@gmail.com>
2024-11-28 00:40:07 -0800

    New changes

--- components/Cards/Products.vue
+++ components/Cards/Products.vue
@@ -1,103 +1,5 @@
 <template>
-  <AppCard
-    data-aos="fade-up"
-    class="app-mx-3 app-mt-3"
-    width="350px"
-    v-if="
-      themeStore.wighet.product.display == 'row' && themeStore.refresh == false
-    "
-  >
-    <template #content>
-      <div class="app-flex">
-        <div style="width: 150px">
-          <v-carousel
-            height="160"
-            hide-delimiters
-            cycle
-            :interval="6000"
-            :show-arrows="false"
-          >
-            <v-carousel
-              height="160"
-              hide-delimiters
-              cycle
-              :interval="6000"
-              :show-arrows="false"
-            >
-              <v-carousel-item v-for="image in data.images" :key="i">
-                <v-sheet height="100%">
-                  <img
-                    class="app-w-100"
-                    style="height: 160px"
-                    :src="image"
-                    alt="image"
-                    loading="lazy"
-                  />
-                </v-sheet>
-              </v-carousel-item>
-            </v-carousel>
-          </v-carousel>
-        </div>
-        <div
-          class="app-flex app-flex-column app-w-100 app-px-3 app-pt-1"
-          style="width: 200px"
-        >
-          <span class="f-s-12 f-w-600">{{ data.name }}</span>
-          <span class="f-s-10 f-w-600">{{ data.category.Title }}</span>
-
-          <span
-            v-if="data.discount > 1"
-            class="bg-danger-100 f-s-12 app-border-radius app-px-2 app-flex app-align-center app-justify-center"
-            style="width: 80px"
-          >
-            % {{ data.discount }} {{ $t("discount") }}
-          </span>
-          <div class="app-flex app-justify-end" v-if="data.discount > 1">
-            <span
-              style="auto"
-              class="app-flex app-justify-end app-mt-4 app-w-600 app-border-radius"
-            >
-              <span class="f-s-12 f-w-600"
-                >{{ data.price_after_discount }} {{ $t("toman") }}</span
-              >
-            </span>
-          </div>
-          <div
-            v-else
-            class="app-flex app-flex-column app-justify-end app-align-end app-pt-2"
-          >
-            <span class="f-s-12 f-w-600"
-              >{{ data.price }} {{ $t("toman") }}</span
-            >
-            <div class="app-flex">
-              <IconsStar v-for="n in 5" />
-            </div>
-          </div>
-          <AppButton
-            :name="$t('order product')"
-            background="bg-primary-100"
-            height="30px"
-            class="app-mt-2"
-            :class="{ 'app-mt-6': data.discount == 0 }"
-            @click="getProduct"
-          >
-            <template #iconLeft>
-              <IconsShoppingCard></IconsShoppingCard>
-            </template>
-          </AppButton>
-        </div>
-      </div> </template
-  ></AppCard>
-
-  <AppCard
-    data-aos="fade-up"
-    class="app-mx-3 app-mt-3"
-    width="300px"
-    v-if="
-      themeStore.wighet.product.display == 'column' &&
-      themeStore.refresh == false
-    "
-  >
+  <AppCard data-aos="fade-up" class="app-mx-3 app-mt-3" width="300px">
     <template #content>
       <div class="app-flex flex-column">
         <v-carousel
@@ -175,9 +77,7 @@
 
 <script setup>
 import { useProductStore } from "~/store/admin/product";
-import { useThemeStore } from "@/store/theme/index";
 
-const themeStore = useThemeStore();
 const productStore = useProductStore();
 
 const props = defineProps({
